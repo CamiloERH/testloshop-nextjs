9028fd22ae1418324c77614c65c4b8018e34aeb5
Camilo Rojas <camilo.rojas1@mail.udp.cl>
2023-08-31 03:46:55 -0400

    nextconfig modified

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    modularizeImports: {
-      "@mui/material": {
-        transform: "@mui/material/{{member}}",
-      },
-      "@mui/icons-material": {
-        transform: "@mui/icons-material/{{member}}",
-      },
-    },
-  },
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
